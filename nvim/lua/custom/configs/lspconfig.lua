e8a38cf99e9dcec13132fd5150fe60857db4aa4b
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2023-12-04 17:25:55 +0300

    Major update for niggas

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -16,5 +16,46 @@ lspconfig.clangd.setup({
     client.server_capabilities.signatureProvider = false
     on_attach(client, bufnr)
   end,
-  capabilities = capabilities
+  capabilities = capabilities,
+  cmd = {"clangd", "--offset-encoding=utf-16", "-std=c++23"}
 })
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+lspconfig.tsserver.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    init_options = {
+      preferences = {
+        disableSuggestions = true,
+      }
+    },
+    commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = "Organize Imports",
+      }
+    }
+})
+
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {"gopls"},
+    filetypes = { "go" , "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+            unusedparams = true,
+        }
+    }
+
+}
---
+++ setup.sh
@@ -0,0 +1,3 @@
+cp hypr mako neofetch nvim swaync waybar wob wofi ~/.config
+cp .zshrc ~/
+gsettings set org.gnome.desktop.interface gtk-theme Dracula
