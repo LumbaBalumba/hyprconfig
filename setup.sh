b865eb40032be860b481991aefdd6fd782e40a94
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2023-12-17 13:57:45 +0300

    yyyy

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 cp qt5ct alacritty hypr mako neofetch nvim swaync waybar wob wofi ~/.config -r
 cp .zshrc ~/
+cp .clang-format ~/
 gsettings set org.gnome.desktop.interface gtk-theme Dracula
--- sync-to-git.sh
+++ sync-to-git.sh
@@ -1,1 +1,3 @@
 cp ~/.config/alacritty ~/.config/hypr ~/.config/mako ~/.config/neofetch/ ~/.config/nvim ~/.config/swaync ~/.config/waybar/ ~/.config/wob ~/.config/wofi ~/.config/btop . -r
+cp ~/.clang-format .
+cp ~/.zshrc .
