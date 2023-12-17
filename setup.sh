81dfdcec6e24157e2c774eef5ca75d04d496b431
i3alumba <LumbaBalumba>
2023-12-17 13:59:02 +0300

    idk lol

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-cp qt5ct alacritty hypr mako neofetch nvim swaync waybar wob wofi ~/.config -r
+cp nvim qt5ct alacritty hypr mako neofetch swaync waybar wob wofi ~/.config -r
 cp .zshrc ~/
-gsettings set org.gnome.desktop.interface gtk-theme Dracula
+#gsettings set org.gnome.desktop.interface gtk-theme Dracula
