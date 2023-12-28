b82989173280a0e025814420e7dffa81a0e90271
i3alumba <LumbaBalumba>
2023-12-28 13:46:53 +0300

    .

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 yay -S - < yay-packages
-cp nvim qt5ct alacritty ranger hypr mako neofetch swaync waybar wob wofi /home/i3alumba/.config -r
+cp nvim qt5ct kitty ranger hypr mako neofetch swaync waybar wob wofi /home/i3alumba/.config -r
 cp .zshrc /home/i3alumba/
 cp .clang-format /home/i3alumba/
 gsettings set org.gnome.desktop.interface gtk-theme Dracula
