123c038cb954cfbf59f837d8255b86820f789467
i3alumba <LumbaBalumba>
2024-01-11 15:12:40 +0300

    fixing code style

--- setup.sh
+++
@@ -1,7 +0,0 @@
-yay -S - < yay-packages
-cp alacritty nvim qt5ct kitty ranger hypr mako neofetch swaync waybar wob wofi btop /home/i3alumba/.config -r
-cp .zshrc /home/i3alumba/
-cp .clang-format /home/i3alumba/
-cp .p10k.zsh /home/i3alumba
-cp .oh-my-zsh /home/i3alumba/ -r
-gsettings set org.gnome.desktop.interface gtk-theme Dracula
